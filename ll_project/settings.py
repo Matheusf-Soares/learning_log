591a709ad9fe5b0b55687354cae9a919656dd4c1
Matheusf-Soares <matheusfps27@icloud.com>
2024-02-29 12:54:38 -0300

    Criado modelos de Entry e Topic, além do site Admin

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Minhas aplicações
+    'learning_logs',
+
+    # Aplicações default do Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
