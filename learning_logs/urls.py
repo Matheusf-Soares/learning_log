b7abb64d9fdcd6d78ba5cba047da9712e917efae
Matheusf-Soares <matheusfps27@icloud.com>
2024-03-04 14:33:11 -0300

    Criada a página de Topics

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""Define padrões de URL para learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Página inicial
+    path('', views.index, name='index'),
+    # Página que mostra todos os tópicos
+    path('topics/', views.topics, name='topics'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+def index(request):
+    """A pátina inicial para o Registro de Aprendizagem"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os tópicos"""
+    topics = Topic.objects.order_by('date_added')
+    context= {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
